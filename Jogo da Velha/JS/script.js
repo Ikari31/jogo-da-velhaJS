a2b0f2c52558aa66f57ce8bd9e2daabcb45d0d68
Guilherme Veiga <106276592+Ikari31@users.noreply.github.com>
2023-05-18 01:44:33 -0300

    Add files via upload

---
+++ Jogo da Velha/JS/script.js
@@ -0,0 +1,91 @@
+let turnPlayer = '';
+const boardRegions = document.querySelectorAll('#tabuleiro span');
+let vBoard = [];
+const button = document.getElementById('start');
+
+function updateTurnPlayer(){
+    const playerInput = document.getElementById(turnPlayer);
+    document.getElementById('turn-player').innerText = playerInput.value;
+}
+
+function initializeGame(){
+    vBoard = [['','',''],['','',''],['','','']];
+    turnPlayer = 'player1';
+    document.querySelector('h2').innerHTML = "Vez de: <span id ='turn-player'></span>";
+    updateTurnPlayer();
+
+    boardRegions.forEach((element) =>{
+        element.classList.remove('win');
+        element.innerText = '';
+
+        element.classList.add('cursor-pointer');
+        element.addEventListener('click',handleBoardClick);
+    });
+}
+
+function disableRegion(element){
+    element.classList.remove('cursor-pointer');
+    element.removeEventListener('click',handleBoardClick);
+}
+
+function getWinRegions() {
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+      winRegions.push("0.0", "0.1", "0.2")
+    if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+      winRegions.push("1.0", "1.1", "1.2")
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+      winRegions.push("2.0", "2.1", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+      winRegions.push("0.0", "1.0", "2.0")
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+      winRegions.push("0.1", "1.1", "2.1")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+      winRegions.push("0.2", "1.2", "2.2")
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+      winRegions.push("0.0", "1.1", "2.2")
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+      winRegions.push("0.2", "1.1", "2.0")
+    return winRegions
+}
+
+function handleBoardClick(ev){
+    const span = ev.currentTarget;
+    const region = span.dataset.region;
+    const position = region.split('.');
+    const row = position[0];
+    const column = position[1];
+
+    if(turnPlayer === 'player1'){
+        span.innerText = 'X';
+        vBoard[row][column] = 'X'
+    }else{
+        span.innerText = 'O';
+        vBoard[row][column] = 'O'
+    }
+
+    console.table(vBoard);
+
+    disableRegion(span);
+
+    let winRegions = getWinRegions();
+
+    if (winRegions.length > 0) {
+        handleWin(winRegions);
+      } else if (vBoard.flat().includes('')) {
+        turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+        updateTurnPlayer();
+      } else {
+        document.querySelector('h2').innerHTML = 'Empate!';
+      }
+}
+
+function handleWin(regions){
+    regions.forEach((region)=>{
+        document.querySelector('[data-region="' + region + '"]').classList.add('win');
+    });
+    const playerName = document.getElementById(turnPlayer).value;
+    document.querySelector('h2').innerHTML = `Jogador ${playerName} venceu!`;
+}
+
+button.addEventListener('click', initializeGame);
